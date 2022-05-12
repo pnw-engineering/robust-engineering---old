4afed012e51c5355b0dd55d63502e831be34bfd7
rickbntn <rbishop461@gmail.com>
2022-05-11 19:36:32 -0700

    Work on fromatting speech page

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,72 +27,38 @@ function toggleMenu() {
 }
 
 function clearBody() {
-  // const topics = document.getElementsByClassName("topic");
-  // for (var i = 0; topics[i]; i++) {
-  //   topics[i].style.display = "none";
-  // }
-  // const element = document.getElementsByClassName("grid-item__title");
-  // for (var i = 0; element[i]; i++) {
-  //   element[i].style.display = "none";
-  // }
   document.getElementById("gb1").style.display = "none";
 }
 
 function restoreBody() {
-  // const topics = document.getElementsByClassName("topic");
-  // for (var i = 0; topics[i]; i++) {
-  //   topics[i].style.border = "1px solid";
-  //   topics[i].style.display = "block";
-  // }
-  // const heads = document.getElementsByClassName("topic__heading");
-  // for (var i = 0; heads[i]; i++) {
-  //   heads[i].style.display = "flex";
-  // }
-  // const sb = document.getElementsByClassName("topic__soundbite");
-  // for (var i = 0; sb[i]; i++) {
-  //   sb[i].style.display = "flex";
-  // }
-  // const img = document.getElementsByClassName("topic__img");
-  // for (var i = 0; img[i]; i++) {
-  //   img[i].style.display = "block";
-  // }
-  // const element = document.getElementsByClassName("grid-item__title");
-  // for (var i = 0; element[i]; i++) {
-  //   element[i].style.display = "block";
-  // }
   document.getElementById("gb1").style.display = "grid";
 }
 
 function showSpeech(s) {
   clearBody();
-  document.getElementById("sp").style.display = "grid";
-  // fetch("/dist/"+s+".html")
+  document.getElementById("sp").style.display = "flex";
   fetch(s+".html")
     .then((response) => response.text())
     .then((text) => (document.getElementById("sp").innerHTML = text));
-  // document.getElementById("b1").innerHTML = text;
 }
 
 function endSpeech() {
   restoreBody();
   document.getElementById("sp").style.display = "none";
-  // document.getElementById("s1").style.display = "none";
   speech = document.getElementsByClassName("ElSpeech");
   for (var i = 0; speech[i]; i++) {
     topics[i].style.display = "none";
   }
-
-  document.getElementById("b2").style.display = "none";
 }
 
-function toggleSpeech() {
-  var x = document.getElementById("s1");
-  if (x.style.display === "none") {
-    x.style.display = "inline-block";
-  } else {
-    x.style.display = "none";
-  }
-}
+// function toggleSpeech() {
+//   var x = document.getElementById("s1");
+//   if (x.style.display === "none") {
+//     x.style.display = "inline-block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
 
 function toggleNav() {
   if (document.getElementById("mySidenav").style.width != "8em") {
