4a34f47140f71f541f3276e3ad1e4da96ca2aba2
rickbntn <rbishop461@gmail.com>
2022-06-16 03:07:30 -0700

    Added images, resructured es# files to load on top of start.html

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,3 +17,32 @@ function closeNav() {
   document.getElementById("mysite").style.gridTemplateColumns = "0 1fr";
 }
 function showIntro() {}
+
+function generateHslaColors (saturation, lightness, alpha, amount) {
+  let colors = []
+  let huedelta = Math.trunc(360 / amount)
+
+  for (let i = 0; i < amount; i++) {
+    let hue = i * huedelta
+    colors.push(`hsla(${hue},${saturation}%,${lightness}%,${alpha})`)
+  }
+
+  return colors
+}
+
+const root = document.querySelector(':root');
+const slider = document.querySelector('input');
+slider.addEventListener('input', function(){
+  // button.addEventListener('input', function(){
+    saturation=root.style.getPropertyValue('--sat');
+    lightness=root.style.getPropertyValue('--light');
+    hue=root.style.getPropertyValue('--hue');
+    // root.style.setProperty("--sat", "${slider.value/100}% );
+    document.getElementById("value").innerHTML= "${sat}%";
+  });
+
+  function loadHTML(fileToLoad){
+    fetch(fileToLoad)
+    .then(response=> response.text())
+    .then(text=> document.getElementById('myContent').innerHTML = text);
+  }
