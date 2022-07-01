12037465ff26cbf6c4666658211223ed9528943f
Rick Bishop <rbishop4561@gmail.com>
2022-06-30 18:04:58 -0700

    Work on hypothesis roadmap page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
-
 function toggleNav() {
-  if (document.getElementById("mysite").style.gridTemplateColumns == "8rem 1fr") {
+  if (
+    document.getElementById("mysite").style.gridTemplateColumns == "8rem 1fr"
+  ) {
     closeNav();
   } else {
     openNav();
@@ -16,33 +17,47 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("mysite").style.gridTemplateColumns = "0 1fr";
 }
+
 function showIntro() {}
 
-function generateHslaColors (saturation, lightness, alpha, amount) {
-  let colors = []
-  let huedelta = Math.trunc(360 / amount)
-
-  for (let i = 0; i < amount; i++) {
-    let hue = i * huedelta
-    colors.push(`hsla(${hue},${saturation}%,${lightness}%,${alpha})`)
-  }
-
-  return colors
+function loadHTML(fileToLoad) {
+  fetch(fileToLoad)
+    .then((response) => response.text())
+    .then((text) => (document.getElementById("myContent").innerHTML = text));
 }
 
-const root = document.querySelector(':root');
-const slider = document.querySelector('input');
-slider.addEventListener('input', function(){
-  // button.addEventListener('input', function(){
-    saturation=root.style.getPropertyValue('--sat');
-    lightness=root.style.getPropertyValue('--light');
-    hue=root.style.getPropertyValue('--hue');
-    // root.style.setProperty("--sat", "${slider.value/100}% );
-    document.getElementById("value").innerHTML= "${sat}%";
-  });
-
-  function loadHTML(fileToLoad){
-    fetch(fileToLoad)
-    .then(response=> response.text())
-    .then(text=> document.getElementById('myContent').innerHTML = text);
+function sibsOff(element) {
+  var sibs = [];
+  el = element;
+  while ((el = el.previousSibling)) {
+    sibs.push(el);
   }
+  el = element;
+  while ((el = el.nextSibling)) {
+    sibs.push(el);
+  }
+
+  while (sibs) {
+    // sibs[0].style.display="none";
+    el = sibs.pop();
+    alert(el);
+    el.style.display = "none";
+  }
+}
+
+function toggleElement(i, j, k) {
+  el = document.getElementById(i);
+  if (el.style.display == "none") {
+    el.style.display = "block";
+    // event.target.style.backgroundColor = 'red';
+  } else {
+    el.style.display = "none";
+    // event.target.style.backgroundColor = 'blue';
+  }
+  document.getElementById(j).style.display = "none";
+  if (document.getElementById(k)) {
+    document.getElementById(k).style.display = "none";
+  }
+  // alert(--accentColor);
+  document.getElementById("h-roadmap").scrollIntoView(false);
+}
