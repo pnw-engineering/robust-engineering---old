899867a01beac3809c49a6cc79813adba5a1b68a
Rick Bishop <rbishop4561@gmail.com>
2022-07-06 12:58:53 -0700

    final tweeks before feature branch

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,5 @@
+addEventListener("onclick", toggleElement);
+
 function toggleNav() {
   if (
     document.getElementById("mysite").style.gridTemplateColumns == "8rem 1fr"
@@ -24,6 +26,27 @@ function loadHTML(fileToLoad) {
   fetch(fileToLoad)
     .then((response) => response.text())
     .then((text) => (document.getElementById("myContent").innerHTML = text));
+}
+
+function clearSubtree(el) {
+  elSet = el.getElementsByClassName("MapLine");
+  for (var i = 0; i < elSet.length; i++) {
+    if (elSet[i].style.display == "block") {
+    console.log(elSet[i].id, " turned OFF - display was:", elSet[i].style.display);
+	elSet[i].style.display = "none";
+	btn = "b" + elSet[i].id;
+	// console.log("trying button ID", btn)
+	if (document.getElementById(btn).style.boxShadow != "none"){
+		document.getElementById(btn).style.boxShadow = "none";
+	}
+  }
+//   bSet = el.getElementsByClassName("MapButton");
+//   for (var i = 0; i < bSet.length; i++) {
+//     if (bSet[i].style.boxShadow != "none") {
+//       elSet[i].style.boxShadow = "none";
+// 	  console.log("button", b, " turned OFF");
+//     }
+  }
 }
 
 function MapButtonOff(btn) {
@@ -67,19 +90,26 @@ function sibsOff(i) {
   }
 }
 
-function toggleElement(i, j, k) {
+function toggleElement() {
+  // console.log(event, "was passed in")
   b = event.target.id;
   i = b.substring(1, b.length);
   el = document.getElementById(i);
-  if (el.style.display == "none") {
+//   console.log("button", b, "was pressed and element", el.id, "selected");
+//   console.log("display status is:", el.style.display);
+  if (el.style.display != "block") {
     el.style.display = "block";
-    document.getElementById(b).style.boxShadow = "0px 0px 5px 5px red";
-    console.log(i, " turned on");
+    document.getElementById(b).style.boxShadow = "0px 0px 3px 3px red";
+    // console.log(i, " turned ON");
   } else {
     el.style.display = "none";
     document.getElementById(b).style.boxShadow = "none";
+    console.log(i, "and", b, " turned OFF");
+	clearSubtree(el);
   }
   sibsOff(i);
+//   console.log("siblings base =", i.substring(0,1))
+  sibsOff(i.substring(0,1))
 
   document
     .getElementById("h-roadmap")
